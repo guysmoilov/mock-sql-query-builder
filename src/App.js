d01bdcfd0548fe314701447e3111e382c9721a89
Guy Smoilovsky <guy@dagshub.com>
2023-08-08 12:46:23 +0300

    Nested conditions work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Condition from './Condition';
+import React, { useState } from 'react';
 
 function App() {
+  const [rootCondition, setRootCondition] = useState({
+    type: 'AND',
+    conditions: []
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SQL WHERE Editor</h1>
+      <Condition condition={rootCondition} onChange={setRootCondition} />
+      <pre>{JSON.stringify(rootCondition, null, 2)}</pre>
     </div>
   );
 }
---
+++ src/Condition.js
@@ -0,0 +1,105 @@
+import React from 'react';
+
+const operators = ['=', '!=', '<', '>', '<=', '>='];
+
+const Condition = ({ condition, onChange, level = 0 }) => {
+  const containerStyle = {
+    border: '1px solid black',
+    padding: '10px',
+    margin: '10px 0',
+    marginLeft: `${20 * level}px`  // Indentation based on nesting level
+  };
+
+  if (!condition.type) {
+    // Simple condition
+    return (
+      <div style={containerStyle}>
+        <input
+          value={condition.field || ''}
+          onChange={e => onChange({ ...condition, field: e.target.value })}
+          placeholder="Field"
+        />
+        <select
+          value={condition.operator || '='}
+          onChange={e => onChange({ ...condition, operator: e.target.value })}
+        >
+          {operators.map(op => <option key={op} value={op}>{op}</option>)}
+        </select>
+        <input
+          value={condition.value || ''}
+          onChange={e => onChange({ ...condition, value: e.target.value })}
+          placeholder="Value"
+        />
+      </div>
+    );
+  } else if (condition.type === 'NOT') {
+    return (
+      <div style={containerStyle}>
+        NOT
+        <Condition
+          condition={condition.condition}
+          onChange={newCond => onChange({ ...condition, condition: newCond })}
+          level={level + 1}
+        />
+      </div>
+    );
+  } else {
+    // Compound condition (AND/OR)
+    return (
+      <div style={containerStyle}>
+        <select
+          value={condition.type}
+          onChange={e => onChange({ ...condition, type: e.target.value })}
+        >
+          <option value="AND">AND</option>
+          <option value="OR">OR</option>
+        </select>
+        <div>
+          {condition.conditions.map((cond, index) => (
+            <div key={index}>
+              <Condition
+                condition={cond}
+                onChange={newCond => {
+                  const newConditions = [...condition.conditions];
+                  newConditions[index] = newCond;
+                  onChange({ ...condition, conditions: newConditions });
+                }}
+                level={level + 1}
+              />
+              <button onClick={() => {
+                const newConditions = [...condition.conditions];
+                newConditions.splice(index, 1);
+                onChange({ ...condition, conditions: newConditions });
+              }}>
+                Remove
+              </button>
+            </div>
+          ))}
+          <button onClick={() => onChange({
+            ...condition, conditions: [...condition.conditions, { field: '', operator: '=', value: '' }]
+          })}>
+            Add Condition
+          </button>
+          <button onClick={() => onChange({
+            ...condition, conditions: [...condition.conditions, { type: 'AND', conditions: [] }]
+          })}>
+            Add AND Group
+          </button>
+          <button onClick={() => onChange({
+            ...condition, conditions: [...condition.conditions, { type: 'OR', conditions: [] }]
+          })}>
+            Add OR Group
+          </button>
+          <button onClick={() => onChange({
+            ...condition, conditions: [...condition.conditions, { type: 'NOT', condition: { field: '', operator: '=', value: '' } }]
+          })}>
+            Add NOT Condition
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Condition;
+
